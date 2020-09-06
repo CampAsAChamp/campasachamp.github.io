af9c278cd48ed24ec602da1c014ead7872442dde
Nick Schneider <nickschneider101@gmail.com>
2020-09-06 08:59:17 -0700

    Added modal for contact me button

--- index.js
+++ index.js
@@ -33,3 +33,35 @@ function scrollToTop() {
     document.documentElement.scrollTop = 0;
     mybutton.style.opacity = 0;
 }
+
+window.onload = function () {
+    // your code
+
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("contact-me-button");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        console.log("Opened");
+        modal.style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        console.log("Closed");
+
+        modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+};
