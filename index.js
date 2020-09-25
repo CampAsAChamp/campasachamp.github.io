12bb0035de9cf1e53782e1aab163add333f38adb
Nick Schneider <nickschneider101@gmail.com>
2020-09-25 15:51:22 -0700

    Added locking of page when nav bar is open

--- index.js
+++ index.js
@@ -76,11 +76,26 @@ const navSlide = () => {
         });
     }
 
+    var toggle = new Boolean(true);
+
     navLinks.forEach(addClickListener);
     burger.addEventListener("click", () => {
         // Toggle Nav
         nav.classList.toggle("nav-active");
 
+        if (toggle) {
+            // When the modal is shown, we want a fixed body
+            document.body.style.position = "fixed";
+            document.body.style.top = `-${window.scrollY}px`;
+            toggle = false;
+        } else {
+            // When the modal is hidden...
+            const scrollY = document.body.style.top;
+            document.body.style.position = "";
+            document.body.style.top = "";
+            window.scrollTo(0, parseInt(scrollY || "0") * -1);
+            toggle = true;
+        }
         // Animate links
         // navLinks.forEach((link, index) => {
         //     link.style.animation = `navLinkFade 0.5s ease forwards ${index / 4 + 0.25}s`;
