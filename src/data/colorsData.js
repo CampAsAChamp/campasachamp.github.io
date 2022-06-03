cc5be6291ef3cb3696bae8c2831c63379b4f74ce
Nick Schneider <nickschneider101@gmail.com>
2022-06-02 21:39:27 -0700

    Add colorsData file

---
+++ src/data/colorsData.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+  PURPLE: "#6c63ff",
+  OFF_GREY: "#707070",
+  YELLOW: "#f2d541",
+  ARCULES: "#0cb4df",
+  REAZON: "#00acee",
+  RAYTHEON: "#ce1126",
+  ID_TECH: "#006633",
+};
