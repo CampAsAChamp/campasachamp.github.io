e8b55d7054bd3f457854156da904c2735b1beb1a
Nick Schneider <nickschneider101@gmail.com>
2022-06-03 13:09:47 -0700

    Add website links to each technology

--- src/data/technologiesData.js
+++ src/data/technologiesData.js
@@ -58,110 +58,137 @@ const technologies = [
   {
     name: ADOBE_ILLUSTRATOR,
     image: Adobe_Illustrator_Icon,
+    link: "https://www.adobe.com/products/illustrator.html",
   },
   {
     name: ADOBE_PHOTOSHOP,
     image: Adobe_Photoshop_Icon,
+    link: "https://www.adobe.com/products/photoshop.html",
   },
   {
     name: ADOBE_XD,
     image: Adobe_XD_Icon,
+    link: "https://www.adobe.com/products/xd.html",
   },
   {
     name: ANGULAR,
     image: Angular_Icon,
+    link: "https://angular.io/",
   },
   {
     name: C,
     image: C_Icon,
-  },
-  {
-    name: CPP,
-    image: CPP_Icon,
-  },
-  {
-    name: CSS3,
-    image: CSS3_Icon,
-  },
-  {
-    name: DOCKER,
-    image: Docker_Icon,
-  },
-  {
-    name: EXPRESS,
-    image: Express_Icon,
-  },
-  {
-    name: FIGMA,
-    image: Figma_Icon,
-  },
-  {
-    name: GCP,
-    image: GCP_Icon,
-  },
-  {
-    name: GIT,
-    image: Git_Icon,
-  },
-  {
-    name: GO,
-    image: Go_Icon,
-  },
-  {
-    name: HTML5,
-    image: HTML5_Icon,
-  },
-  {
-    name: JAVA,
-    image: Java_Icon,
-  },
-  {
-    name: JAVASCRIPT,
-    image: JavaScript_Icon,
-  },
-  {
-    name: KUBERNETES,
-    image: Kubernetes_Icon,
-  },
-  {
-    name: LINUX,
-    image: Linux_Icon,
-  },
-  {
-    name: MYSQL,
-    image: MySQL_Icon,
-  },
-  {
-    name: NODEJS,
-    image: NodeJS_Icon,
-  },
-  {
-    name: NUMPY,
-    image: NumPy_Icon,
-  },
-  {
-    name: POSTGRES,
-    image: Postgres_Icon,
-  },
-  {
-    name: PYTHON,
-    image: Python_Icon,
-  },
-  {
-    name: REACT,
-    image: React_Icon,
-  },
-  {
-    name: REDIS,
-    image: Redis_Icon,
-  },
-  {
-    name: REDUX,
-    image: Redux_Icon,
+    link: "http://www.open-std.org/jtc1/sc22/wg14/",
   },
   {
     name: COLDFUSION,
     image: ColdFusion_Icon,
+    link: "https://www.adobe.com/products/coldfusion-family.html",
+  },
+  {
+    name: CPP,
+    image: CPP_Icon,
+    link: "https://isocpp.org/",
+  },
+  {
+    name: CSS3,
+    image: CSS3_Icon,
+    link: "https://www.w3.org/TR/CSS/#css",
+  },
+  {
+    name: DOCKER,
+    image: Docker_Icon,
+    link: "https://www.docker.com/",
+  },
+  {
+    name: EXPRESS,
+    image: Express_Icon,
+    link: "https://expressjs.com/",
+  },
+  {
+    name: FIGMA,
+    image: Figma_Icon,
+    link: "https://www.figma.com/",
+  },
+  {
+    name: GCP,
+    image: GCP_Icon,
+    link: "https://cloud.google.com/",
+  },
+  {
+    name: GIT,
+    image: Git_Icon,
+    link: "https://git-scm.com/",
+  },
+  {
+    name: GO,
+    image: Go_Icon,
+    link: "https://go.dev/",
+  },
+  {
+    name: HTML5,
+    image: HTML5_Icon,
+    link: "https://html.spec.whatwg.org/",
+  },
+  {
+    name: JAVA,
+    image: Java_Icon,
+    link: "https://www.oracle.com/java/",
+  },
+  {
+    name: JAVASCRIPT,
+    image: JavaScript_Icon,
+    link: "http://www.ecma-international.org/publications-and-standards/standards/ecma-262/",
+  },
+  {
+    name: KUBERNETES,
+    image: Kubernetes_Icon,
+    link: "https://kubernetes.io/",
+  },
+  {
+    name: LINUX,
+    image: Linux_Icon,
+    link: "https://www.kernel.org/",
+  },
+  {
+    name: MYSQL,
+    image: MySQL_Icon,
+    link: "https://www.mysql.com/",
+  },
+  {
+    name: NODEJS,
+    image: NodeJS_Icon,
+    link: "https://nodejs.org/",
+  },
+  {
+    name: NUMPY,
+    image: NumPy_Icon,
+    link: "https://numpy.org/",
+  },
+  {
+    name: POSTGRES,
+    image: Postgres_Icon,
+    link: "https://www.postgresql.org/",
+  },
+  {
+    name: PYTHON,
+    image: Python_Icon,
+    link: "https://www.python.org/",
+  },
+  {
+    name: REACT,
+    image: React_Icon,
+    link: "https://reactjs.org/",
+  },
+  {
+    name: REDIS,
+    image: Redis_Icon,
+    link: "https://redis.io/",
+  },
+  {
+    name: REDUX,
+    image: Redux_Icon,
+    link: "https://redux.js.org/",
   },
 ];
 
