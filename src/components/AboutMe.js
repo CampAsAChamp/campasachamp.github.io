184600db19e9b475134f36ee9ba898ff02afe25e
Nick Schneider <nickschneider101@gmail.com>
2022-06-05 12:42:22 -0700

    Adjust about me image animation delays

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,19 +1,19 @@
 import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
 
 import Purple_S_Logo from "assets/Purple S Logo.svg";
 import GradCapIllustration from "assets/undraw_Graduation_ktn0.svg";
 import DeskIllustration from "assets/undraw_feeling_proud_qne1.svg";
-import ScrollAnimation from "react-animate-on-scroll";
 
 export function AboutMe() {
   return (
     <section id="about-me-container" className="page-container">
       <img id="background-logo" src={Purple_S_Logo} alt="Logo Backdrop" />
       <div id="about-me-images">
-        <ScrollAnimation animateIn="animate__backInLeft" animateOnce={true} delay={400}>
+        <ScrollAnimation animateIn="animate__backInLeft" animateOnce={true} delay={100}>
           <img id="grad-cap-illustration" src={GradCapIllustration} alt="Graduation Cap" />
         </ScrollAnimation>
-        <ScrollAnimation animateIn="animate__backInLeft" animateOnce={true} delay={600}>
+        <ScrollAnimation animateIn="animate__backInLeft" animateOnce={true} delay={200}>
           <img id="desk-illustration" src={DeskIllustration} alt="Desk Illustration" />
         </ScrollAnimation>
       </div>
