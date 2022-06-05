6803527b0f09eba6091fdc94c2daa4e321ca1a22
Nick Schneider <nickschneider101@gmail.com>
2022-06-05 12:18:58 -0700

    Add height and width to inline SVG for skills icon

--- src/components/SkillsRow.js
+++ src/components/SkillsRow.js
@@ -17,7 +17,16 @@ export function SkillsRow(props) {
       {technologies.map((tech) => {
         return (
           <div className="skills-icon-container" key={tech.name}>
-            <Svg src={tech.image} fill={COLORS.PURPLE} title={tech.name + " Icon"} alt={tech.name + " Icon"} />
+            <a href="https://www.google.com">
+              <Svg
+                src={tech.image}
+                width={100}
+                height={100}
+                fill={COLORS.PURPLE}
+                title={tech.name + " Icon"}
+                alt={tech.name + " Icon"}
+              />
+            </a>
             <div className="skills-caption">{tech.name}</div>
           </div>
         );
