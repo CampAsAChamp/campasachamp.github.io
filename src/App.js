206fe4719b63be98010e3fea8e3e9a9cd522bef9
Nick Schneider <nickschneider101@gmail.com>
2022-06-05 14:54:44 -0700

    Add theme switcher

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+import DarkModeToggle from "react-dark-mode-toggle";
+
 import { AboutMe } from "components/AboutMe";
 import { ArtProjects } from "components/ArtProjects";
 import { Experience } from "components/Experience";
@@ -8,10 +11,13 @@ import { ScrollToTopButton } from "components/ScrollToTopButton";
 import { SkillsAndTechnologies } from "components/SkillsAndTechnologies";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(() => false);
+
   return (
     <>
       <Navbar />
       <ScrollToTopButton />
+      <DarkModeToggle className="theme-switcher" onChange={setIsDarkMode} checked={isDarkMode} size={80} />
       <LandingPage />
       <AboutMe />
       <Experience />
