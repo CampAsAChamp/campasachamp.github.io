ca7bbb2f26386cb52ff129161b6b00f3a386aa9d
Nick Schneider <nickschneider101@gmail.com>
2022-06-09 22:59:41 -0700

    Implement mobile styling of navbar/hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import S_Logo from "assets/S_Logo.svg";
 
@@ -7,6 +7,39 @@ import { Svg } from "components/Svg";
 import "styles/Navbar.css";
 
 export function Navbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  function openNavModal() {
+    const burger = document.querySelector(".hamburger-menu");
+    const nav = document.querySelector("nav ul");
+
+    // Open Nav
+    nav.classList.add("nav-active");
+    burger.classList.add("toggle");
+
+    // When the modal is shown, we want a fixed body
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${window.scrollY}px`;
+    setIsNavOpen(true);
+  }
+
+  function closeNavModal() {
+    const burger = document.querySelector(".hamburger-menu");
+    const nav = document.querySelector("nav ul");
+
+    // Close Nav
+    nav.classList.remove("nav-active");
+    burger.classList.remove("toggle");
+
+    document.body.style.position = "";
+    document.body.style.top = "";
+    setIsNavOpen(false);
+  }
+
+  function navSlide() {
+    isNavOpen ? closeNavModal() : openNavModal();
+  }
+
   return (
     <nav>
       <a href="/">
@@ -14,22 +47,32 @@ export function Navbar() {
       </a>
       <ul>
         <li>
-          <a href="#about-me-images">About Me</a>
+          <a href="#about-me-images" onClick={closeNavModal}>
+            About Me
+          </a>
         </li>
         <li>
-          <a href="#experience-header">Experience</a>
+          <a href="#experience-header" onClick={closeNavModal}>
+            Experience
+          </a>
         </li>
         <li>
-          <a href="#skills-header">Skills</a>
+          <a href="#skills-header" onClick={closeNavModal}>
+            Skills
+          </a>
         </li>
         <li>
-          <a href="#graphic-design-header">Art & Design</a>
+          <a href="#graphic-design-header" onClick={closeNavModal}>
+            Art & Design
+          </a>
         </li>
         <li>
-          <a href="#sw-projects-header">Projects</a>
+          <a href="#sw-projects-header" onClick={closeNavModal}>
+            Projects
+          </a>
         </li>
       </ul>
-      <div className="hamburger-menu">
+      <div className="hamburger-menu" onClick={navSlide}>
         <div className="line" id="line1" />
         <div className="line" id="line2" />
         <div className="line" id="line3" />
