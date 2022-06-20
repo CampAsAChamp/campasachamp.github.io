72006304293a40fde5c1f1a8d1b678fab9b3f428
Nick Schneider <nickschneider101@gmail.com>
2022-06-20 15:21:46 -0700

    Center about me header

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -21,7 +21,9 @@ export function AboutMe() {
           </ScrollAnimation>
         </div>
         <div id="about-me-text">
-          <h2>ABOUT ME</h2>
+          <div id="about-me-header">
+            <h2>ABOUT ME</h2>
+          </div>
           <p>Hey I'm Nick, a software engineer based in 🌴🎬 Los Angeles</p>
           <p>
             I graduated from the 🎓 <strong>University of California, Irvine</strong> in 2019 with a{" "}
