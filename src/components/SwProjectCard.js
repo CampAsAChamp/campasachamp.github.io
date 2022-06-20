f102c96c361ad7a9bbb45a19d429cac59795ec1b
Nick Schneider <nickschneider101@gmail.com>
2022-06-20 13:27:24 -0700

    Remove duplicate sw-projects-cards rule and use global card

--- src/components/SwProjectCard.js
+++ src/components/SwProjectCard.js
@@ -13,32 +13,30 @@ export function SwProjectCard(props) {
   const { project, index } = props;
 
   return (
-    <div className="sw-projects-cards" id={"sw-projects-card" + index}>
-      <div className="card-content">
-        <div className="sw-projects-title-container">
-          <div className="sw-project-title card-title">{project.name}</div>
-          <TechnologiesBar technologyNames={project.technologies} fillColor={COLORS.PURPLE} />
+    <div className="card sw-projects-card" id={"sw-projects-card" + index}>
+      <div className="sw-projects-title-container">
+        <div className="sw-project-title card-title">{project.name}</div>
+        <TechnologiesBar technologyNames={project.technologies} fillColor={COLORS.PURPLE} />
+      </div>
+      <div className="sw-projects-content">
+        <div className="sw-projects-thumbnail-container">
+          <img className="sw-projects-thumbnail" src={project.thumbnail} alt={project.name} title={project.name} />
         </div>
-        <div className="sw-projects-content">
-          <div className="sw-projects-thumbnail-container">
-            <img className="sw-projects-thumbnail" src={project.thumbnail} alt={project.name} title={project.name} />
+        <div className="sw-projects-info-container">
+          <div className="sw-projects-text">
+            <ul>{parse(project.textContent)}</ul>
           </div>
-          <div className="sw-projects-info-container">
-            <div className="sw-projects-text">
-              <ul>{parse(project.textContent)}</ul>
-            </div>
-            <div className="sw-projects-button-row">
-              <button
-                type="button"
-                className="button sw-projects-button hvr-wobble-vertical"
-                onClick={() => {
-                  window.open(project.link, "_blank");
-                }}
-              >
-                View Code
-                <Svg className="github-button-icon" src={GithubIcon} fill="white" alt="Github Icon" />
-              </button>
-            </div>
+          <div className="sw-projects-button-row">
+            <button
+              type="button"
+              className="button sw-projects-button hvr-wobble-vertical"
+              onClick={() => {
+                window.open(project.link, "_blank");
+              }}
+            >
+              View Code
+              <Svg className="github-button-icon" src={GithubIcon} fill="white" alt="Github Icon" />
+            </button>
           </div>
         </div>
       </div>
