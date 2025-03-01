769d5ab5f84f79e5badf2e269b9b44fe139956dd
Nick Schneider <nickschneider101@gmail.com>
2025-02-28 22:35:15 -0800

    Add Typescript to Skills and Technologies section

--- src/components/SkillsAndTech/SkillsAndTechnologies.jsx
+++ src/components/SkillsAndTech/SkillsAndTechnologies.jsx
@@ -24,6 +24,7 @@ export function SkillsAndTechnologies() {
         />
         <SkillsRow
           technologyNames={[
+            technologies.TYPESCRIPT,
             technologies.JAVASCRIPT,
             technologies.REACT,
             technologies.REDUX,
