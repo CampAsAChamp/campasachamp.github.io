1f076896c31aaf2e8799921c64de5eb9775db71d
Nick Schneider <nickschneider101@gmail.com>
2025-02-28 22:43:30 -0800

    Rename line to hamburger line to be more clear

--- src/components/NavBar/HamburgerMenu.jsx
+++ src/components/NavBar/HamburgerMenu.jsx
@@ -3,10 +3,9 @@ import React from 'react'
 export function HamburgerMenu({ navSlide }) {
     return (
         <div className="hamburger-menu" onClick={navSlide}>
-            {/* Lines of the literal hamburger mmenu */}
-            <div className="line" id="line1" />
-            <div className="line" id="line2" />
-            <div className="line" id="line3" />
+            <div className="hamburger-menu-line" id="hamburger-menu-line1" />
+            <div className="hamburger-menu-line" id="hamburger-menu-line2" />
+            <div className="hamburger-menu-line" id="hamburger-menu-line3" />
         </div>
     )
 }
